7339a848ecfed66a968c753e3393d51fe7cf2e5e
ms747 <mayur.shah.ha@gmail.com>
2018-09-19 01:10:11 +0530

    Added fontawesome

--- mywork.jsx
+++ mywork.jsx
@@ -11,7 +11,8 @@ let WorkCard = function({ projects }) {
             className="card"
           >
             <h3 className="card-title">{project.name}</h3>
-            <img className="card-img" src={project.src} alt="" />
+            {/* <img className="card-img" src={project.src} alt="" /> */}
+            <i className={project.class} />
             <div className="card-link">
               <p className="link">
                 <i className="fas fa-arrow-right" />
@@ -31,25 +32,29 @@ class App extends React.Component {
         id: 1,
         name: "Recruitment Website for Latur Zilhaparishad",
         url: "http://www.msrlmlaturjobs.com",
-        src: "./img/latur.png"
+        src: "./img/latur.png",
+        class: "fas fa-users card-img"
       },
       {
         id: 2,
         name: "Website for GanSan IT Technologies",
         url: "http://www.gansan.in/",
-        src: "./img/website.png"
+        src: "./img/website.png",
+        class: "fab fa-chrome card-img"
       },
       {
         id: 3,
         name: "Website for Shivasol Technologies",
         url: "http://support.shivasol.com",
-        src: "./img/website.png"
+        src: "./img/website.png",
+        class: "fab fa-chrome card-img"
       },
       {
         id: 4,
-        name: "Website for Shivasol Technologies",
-        url: "http://support.shivasol.com",
-        src: "./img/website.png"
+        name: "My Git Work",
+        url: "https://github.com/ms747/",
+        src: "./img/website.png",
+        class: "fab fa-github card-img"
       }
     ]
   };
