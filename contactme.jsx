620d5eb9d56d400cdc9f2f778154e1d979d1b979
ms747 <mayur.shah.ha@gmail.com>
2018-09-21 12:58:30 +0530

    Fixed: cannot set the value of null from mouseOver event

--- contactme.jsx
+++ contactme.jsx
@@ -1,9 +1,20 @@
 class App extends React.Component {
-  state = {};
+  constructor(props) {
+    super();
+    this.state = {};
+    this.myRef = React.createRef();
+  }
 
   handleForm = e => {
     e.preventDefault();
     console.log("works");
+  };
+
+  handleMouseOver = e => {
+    this.myRef.current.classList.add("popup");
+    setTimeout(_=>{
+      this.myRef.current.classList.remove("popup");
+    },300);
   };
 
   render() {
@@ -18,10 +29,16 @@ class App extends React.Component {
           <label htmlFor="message">Message</label>
           <textarea id="message" type="text" rows="10" cols="50" />
           <div id="button-container">
-            <button type="submit" form="form1" value="Submit" id="submit">
+            <button
+              type="submit"
+              form="form1"
+              value="Submit"
+              id="submit"
+              onMouseOver={this.handleMouseOver}
+            >
               Submit
             </button>
-            <button disabled id="after" />
+            <button disabled id="after" ref={this.myRef} />
           </div>
         </form>
       </div>
