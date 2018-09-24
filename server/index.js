09636efb8f7a5e78bb15d92b99910ac1c43fc48a
root <root@ubuntu.members.linode.com>
2018-09-24 21:05:39 +0530

    HTTPS

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const https= require("https");
 const contactmeRoute = require('./contactme');
 require("dotenv").config();
 
 const MONGO_URL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_COLLECTION}`;
-
+console.log(MONGO_URL);
 mongoose.connect(MONGO_URL, {
 	useNewUrlParser: true
 }, (err) => {
@@ -25,7 +27,12 @@ app.get("/", (req, res) => {
 	res.send("What");
 });
 
-const port = process.env.PRODUCTION || 8080;
+/*const port = process.env.PRODUCTION || 8080;
 app.listen(port, () => {
 	console.log("Listenting");
-});
+});*/
+
+https.createServer({
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.cert')
+},app).listen(8080, () => { console.log("HTTPS server is running") });
